8b4405dd8d9e3464d8feceab328f03ea0a4f4039
unknown <hspreet1998@gmail.com>
2018-08-17 18:17:16 +0530

    add project page

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express=require('express');
 const hbs=require('hbs');
 const fs=require('fs');
 const port=process.env.PORT || 3000;
+
 var app=express();
 
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine','hbs');
+
 
 
 app.use((req,res,next)=>{
@@ -20,9 +22,11 @@ if(err){
 next();
 });
 
-app.use((req,res,next)=>{
-res.render('maintenance.hbs');
-});
+// app.use((req,res,next)=>{
+// res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname+'/public'));
 
 hbs.registerHelper('getCurrentYear',()=>{
 return new Date().getFullYear();
@@ -32,7 +36,7 @@ hbs.registerHelper('screamIt',(text)=>{
 return text.toUpperCase();
 });
 
-app.use(express.static(__dirname+'/public'));
+
 app.get('/',(req,res)=>{
 // res.send({
 // name:'Harpreet Saini',
@@ -50,6 +54,12 @@ app.get('/about',(req,res)=>{
 res.render('about.hbs',{
     pageTitle:'About page',
     currentYear:new Date().getFullYear()
+});
+});
+
+app.get('/projects',(req,res)=>{
+res.render('projects.hbs',{
+    pageTitle:'Projects'
 });
 });
 
